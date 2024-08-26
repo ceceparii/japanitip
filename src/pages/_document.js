80a64064abe7ffca4bf6154831192e6645a45b28
ceceparii <cecepari49@gmail.com>
2024-08-26 07:34:05 +0000

    change redirect

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang='en'>
+            <Head>
+                <meta
+                    httpEquiv='Content-Security-Policy'
+                    content="default-src 'self'; img-src 'self' https://japanitip.vercel.app; font-src 'self'; script-src 'self'; style-src 'self';"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
